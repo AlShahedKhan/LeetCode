c8e2b5417f1d74fd678903ad55838db4459120d3
Abdullah al shahed <abdullahalshahedkhan@gmail.com>
2025-07-06 13:10:47 +0600

    called index

--- 26_Remove_Duplicates_from_Sorted_Array.php
+++ 26_Remove_Duplicates_from_Sorted_Array.php
@@ -1,1 +1,19 @@
 <?php
+
+class Solution {
+    function removeDuplicates(&$nums){
+        if (count($nums) == 0 ) {
+            return 0;
+        }
+
+        $index = 1;
+
+        for ($i=0; $i < count($nums); $i++) { 
+            echo "$index";
+        }
+    }
+}
+
+$nums = [1, 1, 2];
+$solution = new Solution();
+$length = $solution->removeDuplicates($nums);
